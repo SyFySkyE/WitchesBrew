3755cee5133f53e966ac6e15802c085150078394
EfisEvisule <58088743+djstagen@users.noreply.github.com>
2020-10-17 17:46:14 -0500

    Pause Menu Added, Hallow Witch font added

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pause;
+    public bool check = true;
+
+    void Update()
+    {
+        if ((Input.GetKeyDown(KeyCode.P)) && (check == false))
+        {                       
+           OpenPauseMenu();
+           Debug.Log("tick on ");
+        }
+        else if ((Input.GetKeyDown(KeyCode.P)) && (check == true))
+        {
+            ClosePauseMenu();
+            Debug.Log("tick off");
+        }
+    }
+
+    private void OpenPauseMenu()
+    {
+        pause.SetActive(true);
+        Time.timeScale = 0f;
+        check = true;
+    }
+
+    private void ClosePauseMenu()
+    {
+        pause.SetActive(false);
+        Time.timeScale = 1f;
+        check = false;
+
+    }
+}
