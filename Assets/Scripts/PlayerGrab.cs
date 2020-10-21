e73eafb8b45b608d2b288754af77886d290e32a4
Chris Grych <44657835+SyFySkyE@users.noreply.github.com>
2020-10-20 22:02:42 -0500

    Puts in rest of container prefabs, starts GO ingred hovering

--- Assets/Scripts/PlayerGrab.cs
+++ Assets/Scripts/PlayerGrab.cs
@@ -2,9 +2,31 @@
 
 public class PlayerGrab : MonoBehaviour
 {
-    public IngredientEnum heldIngredient;
+    public IngredientEnum HeldIngredient;
+    private GameObject ingredGORep;
+
+    public float zOffset = -1.5f;
 
     private void Update()
+    {
+        CameraRay();
+        Debug.Log(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -5.8f)));
+        if (ingredGORep != null)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Vector3 mousePosToScreen = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+                
+                ingredGORep.transform.position = mousePosToScreen;
+            }
+            else
+            {
+                ingredGORep = null;
+            }
+        }
+    }    
+
+    private void CameraRay()
     {
         Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition); // If I was a real good boi, I would abstract mouse pos so I could easily sub touch func or controller
         RaycastHit hitInfo;
@@ -15,8 +37,9 @@ public class PlayerGrab : MonoBehaviour
             {
                 if (hitInfo.collider.tag == "Ingredient")
                 {
-                    heldIngredient = hitInfo.collider.GetComponent<UnityIngredient>().GetIngredientType();
-                    Debug.Log($"You picked up ingredient: {heldIngredient}");
+                    HeldIngredient = hitInfo.collider.GetComponent<UnityIngredient>().GetIngredientType();
+                    ingredGORep = hitInfo.collider.GetComponent<UnityIngredient>().IngredientGORep;
+                    Debug.Log($"You picked up ingredient: {HeldIngredient}");
                 }
             }
         }
@@ -26,19 +49,19 @@ public class PlayerGrab : MonoBehaviour
             {
                 if (hitInfo.collider.tag == "Cauldron")
                 {
-                    if (this.heldIngredient != IngredientEnum.None)
+                    if (this.HeldIngredient != IngredientEnum.None)
                     {
-                        hitInfo.collider.GetComponent<Cauldron>().AddIngredient(heldIngredient);
-                        Debug.Log($"You put ingredient: {heldIngredient} in cauldron");
-                        heldIngredient = IngredientEnum.None;
+                        hitInfo.collider.GetComponent<Cauldron>().AddIngredient(HeldIngredient);
+                        Debug.Log($"You put ingredient: {HeldIngredient} in cauldron");
+                        HeldIngredient = IngredientEnum.None;
                     }
                 }
                 else
                 {
-                    if (this.heldIngredient != IngredientEnum.None)
+                    if (this.HeldIngredient != IngredientEnum.None)
                     {
-                        Debug.Log($"You dropped ingredient: {heldIngredient}");
-                        heldIngredient = IngredientEnum.None;
+                        Debug.Log($"You dropped ingredient: {HeldIngredient}");
+                        HeldIngredient = IngredientEnum.None;
                     }
                 }
             }
--- Assets/Scripts/UnityIngredient.cs
+++ Assets/Scripts/UnityIngredient.cs
@@ -1,16 +1,99 @@
 ﻿using UnityEngine;
 
-public class UnityIngredient : MonoBehaviour
+public class UnityIngredient : MonoBehaviour // Is responisible for both the container and GO ingred, infringes on proper SoC!
 {
-    [SerializeField] private IngredientEnum ingredient; // Can we dynamically choose our material on Start depending on what this is? Would that be desired?
+    [SerializeField] private IngredientEnum ingredient;
+
+    public GameObject IngredientGORep { get; private set; }
+    private string resourcePath = "IngredientPrefabs/";    
+    private float minYBeforeDisable = -10f; // How far the GO rep can fall before being disabled
 
     private void Start()
     {
         this.tag = "Ingredient";
+        InstantiateIngredient();        
+    }
+
+    private void Update()
+    {
+        if (this.IngredientGORep.activeSelf)
+        {
+            this.IngredientGORep.transform.position = this.transform.position + Physics.gravity * Time.deltaTime; // Look at KP
+        }
+
+        if (this.IngredientGORep.transform.position.y <= minYBeforeDisable)
+        {
+            this.IngredientGORep.transform.position = Vector3.zero;
+            this.IngredientGORep.SetActive(false);
+        }
     }
 
     public IngredientEnum GetIngredientType()
     {
         return this.ingredient;
+    }
+
+    private void OnMouseDown()
+    {
+        IngredientGORep.SetActive(true);
+    }
+
+    private void InstantiateIngredient()
+    {
+        string ingredientToSpawn = string.Empty; // Can't leave this uninitialized
+
+        #region Decide what ingredient to use based on this ingredient type
+
+        switch (this.ingredient)
+        {
+            case IngredientEnum.BabyShoesNeverWorn:
+                ingredientToSpawn = "Baby_Shoes";
+                break;
+
+            case IngredientEnum.Eyeballs:
+                ingredientToSpawn = "Eyeball";
+                break;
+
+            case IngredientEnum.FrogLegs:
+                ingredientToSpawn = "Frog_Legs";
+                break;
+
+            case IngredientEnum.PumpkinSpice:
+                ingredientToSpawn = "Empty";
+                Debug.Log($"GO equavilent of type: {this.ingredient} is not set, setting GO rep to empty");
+                break;
+
+            case IngredientEnum.RatTail:
+                ingredientToSpawn = "Rat_Tail";
+                break;
+
+            case IngredientEnum.Snails:
+                ingredientToSpawn = "Snail";
+                break;
+
+            case IngredientEnum.Tears:
+                ingredientToSpawn = "Empty";
+                Debug.Log($"GO equavilent of type: {this.ingredient} is not set, setting GO rep to empty");
+                break;
+
+            case IngredientEnum.Vanilla:
+                ingredientToSpawn = "Vanilla";
+                break;
+
+            case IngredientEnum.Worms:
+                ingredientToSpawn = "Worms";
+                break;
+
+            default:
+                ingredientToSpawn = "Empty";
+                Debug.Log($"GO equavilent of type: {this.ingredient} is not set, setting GO rep to empty");
+                break;
+        }
+
+        #endregion
+
+        IngredientGORep = Instantiate(Resources.Load<GameObject>(resourcePath + ingredientToSpawn));
+        IngredientGORep.transform.SetParent(this.transform);
+        IngredientGORep.SetActive(false);
     }
 }
