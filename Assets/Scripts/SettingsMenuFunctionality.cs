898c87ea1526962f25922ecda1926d08c6ee22eb
VinylKnight <33336283+VinylKnight@users.noreply.github.com>
2020-10-24 21:34:56 -0500

    Baby Shoes Added To Container

---
+++ Assets/Scripts/SettingsMenuFunctionality.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenuFunctionality : MonoBehaviour
+{
+    [SerializeField]
+    private Dropdown resolutionDropDown;
+    [SerializeField]
+    private Slider volumeSlider;
+    [SerializeField]
+    private Dropdown graphicsDropDown;
+
+    // Start is called before the first frame update
+    public void VolumeAdjustment(float volume)
+    { 
+        
+    }
+    //void Start()
+    //{
+        
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
