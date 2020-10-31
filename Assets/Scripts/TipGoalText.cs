73253d86c074d7081156f655e25cdf0b1c31c417
Charlie Phillips <hi.charliephillips@gmail.com>
2020-10-31 18:13:16 -0500

    added fade in/out goal text

--- Assets/Scripts/TipGoalText.cs
+++ Assets/Scripts/TipGoalText.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -7,11 +8,58 @@ public class TipGoalText : MonoBehaviour
     [SerializeField]
     private TipJar tipJar;
 
+    [SerializeField]
+    private bool fadeTextInAndOut;
+
+    [SerializeField]
+    private float fadeInTime, fadeOutTime = 1f;
+
+    [SerializeField]
+    private float delayBeforeFadeOutTime = 2f;
+
     private TextMeshProUGUI goalText;
 
     private void Start()
     {
         goalText = GetComponent<TextMeshProUGUI>();
-        goalText.text = "Out of: $" + Math.Round(tipJar.tipGoal, 2).ToString();
+
+        if (fadeTextInAndOut)
+        {
+            goalText.text = "Today's tip goal: $" + Math.Round(tipJar.tipGoal, 2).ToString();
+            StartCoroutine(FadeTextInAndOut());
+        }
+        else
+        {
+            goalText.text = "Out of $" + Math.Round(tipJar.tipGoal, 2).ToString();
+        }
     }
+
+    private IEnumerator FadeTextInAndOut()
+    {
+        StartCoroutine(FadeTextToFullAlpha(fadeInTime, goalText));
+        yield return new WaitForSeconds(delayBeforeFadeOutTime);
+        StartCoroutine(FadeTextToZeroAlpha(fadeOutTime, goalText));
+    }
+
+    private IEnumerator FadeTextToFullAlpha(float t, TextMeshProUGUI i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
+        while (i.color.a < 1.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / t));
+            yield return null;
+        }
+    }
+
+    private IEnumerator FadeTextToZeroAlpha(float t, TextMeshProUGUI i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
+        while (i.color.a > 0.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
+            yield return null;
+        }
+    }
+
 }
+
