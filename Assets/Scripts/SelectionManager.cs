e18ca991ca8067780e548746783155d9b182e86f
AlexanderEnriquezCCC <44684320+AlexanderEnriquezCCC@users.noreply.github.com>
2020-11-13 12:33:32 -0600

    adds the quickoutline assets

---
+++ Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,32 @@
+﻿using MK.Toon;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour
+{
+    [SerializeField]
+    private string selectableTag = "Selectable";
+
+    [SerializeField]
+    private Material highlightMaterial;
+
+    [SerializeField]
+    private Material defaultMaterial;
+
+    private Transform _selection;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (_selection != null)
+        {
+            
+        }
+    }
+}
