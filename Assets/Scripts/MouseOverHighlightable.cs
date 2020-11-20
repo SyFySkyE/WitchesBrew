686e7b3d72fb695ae9dc12a275a7eff08007ead2
AlexanderEnriquezCCC <44684320+AlexanderEnriquezCCC@users.noreply.github.com>
2020-11-19 20:15:52 -0600

    Highlight test works on MouseOver/Exit

---
+++ Assets/Scripts/MouseOverHighlightable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOverHighlightable : MonoBehaviour
+{
+    [SerializeField]
+    Material highlightMaterial;
+
+    private Material defaultMaterial;
+    private MeshRenderer meshRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        defaultMaterial = meshRenderer.material;
+    }
+
+    private void OnMouseOver()
+    {
+        meshRenderer.material = highlightMaterial;
+    }
+
+    private void OnMouseExit()
+    {
+        meshRenderer.material = defaultMaterial;
+    }
+}
