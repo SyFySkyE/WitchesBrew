9ce67a63c57917c49e1d3a1179d0206ee16c9580
Charlie Phillips <hi.charliephillips@gmail.com>
2020-11-20 12:39:45 -0600

    cleared console warnings editor

--- Assets/Scripts/TipJar.cs
+++ Assets/Scripts/TipJar.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.UI;
 
 /// <summary>
 /// Calculates the tip for each order upon its completion and adds it to the tip total
